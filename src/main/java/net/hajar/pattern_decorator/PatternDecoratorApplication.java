abe15be7689c1f631d6ac8e9fa098e7644423e25
HAJAR ATYQ <hajar.atyq.pro@gmail.com>
2025-12-24 23:44:59 +0100

    FC

---
+++ src/main/java/net/hajar/pattern_decorator/PatternDecoratorApplication.java
@@ -0,0 +1,13 @@
+package net.hajar.pattern_decorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternDecoratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternDecoratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hajar/pattern_decorator/PatternDecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hajar.pattern_decorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternDecoratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
